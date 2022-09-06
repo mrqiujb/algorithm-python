65240ab9d5aaf99fe8be16822d466801bfa255c6
qiujingbao <1903094657@qq.com>
2022-09-06 23:15:03 +0800

    test2

--- src/union_find/common.py
+++ src/union_find/common.py
@@ -1,5 +1,6 @@
 #coding=UTF-8
 #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
+#git remote set-url origin https://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@github.com/1903094657@qq.com/algorithm-python.git
 class common():
     """类的功能说明"""
     def mul(self,s,b):
