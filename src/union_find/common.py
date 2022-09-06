3f595aa724141c5064676ef3890f2336d2e296ac
qiujingbao <1903094657@qq.com>
2022-09-06 22:39:17 +0800

    sphinx初始化 文档目录初始化

---
+++ src/union_find/common.py
@@ -0,0 +1,48 @@
+#coding=UTF-8
+class common():
+    """类的功能说明"""
+    def sub(self,f,x):
+        """
+        :param f:
+        :param x:
+        :return:
+        """
+        return self
+    def add(self,a,b):
+        """两个数字相加，并返回结果"""
+        return a+b
+
+    def google_style(arg1, arg2):
+        """函数功能.
+
+        函数功能说明.
+
+        Args:
+            arg1 (int): arg1的参数说明
+            arg2 (str): arg2的参数说明
+
+        Returns:
+            bool: 返回值说明
+
+        """
+        return True
+
+    def numpy_style(arg1, arg2):
+        """函数功能.
+
+        函数功能说明.
+
+        Parameters
+        ----------
+        arg1 : int
+            arg1的参数说明
+        arg2 : str
+            arg2的参数说明
+
+        Returns
+        -------
+        bool
+            返回值说明
+
+        """
+        return True
