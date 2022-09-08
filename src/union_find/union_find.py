ea6eabe30d87ca628f551ab1c43b7c40adb37f60
qiujingbao <1903094657@qq.com>
2022-09-08 11:05:47 +0800

    添加quick-union

--- src/union_find/union_find.py
+++ src/union_find/union_find.py
@@ -1,4 +1,4 @@
-class UnionFind(object):
+class UnionFindQuickFind(object):
     def __init__(self, N: int):
         """
         初始化并查集
@@ -77,8 +77,87 @@ class UnionFind(object):
                     flags[j] = 1
 
 
+class UnionFindQuickUnion(object):
+    def __init__(self, N: int):
+        """
+        初始化并查集
+        :param N: 元素的个数
+        """
+        self.count = N
+        self.sets = N
+        self.elements = []
+        for i in range(0, N):
+            self.elements.append(-1)
+
+    def get_count(self) -> int:
+        """
+        返回当前元素个数
+        :return:
+        """
+        return self.count
+
+    def get_sets(self) -> int:
+        """
+        返回分量的个数
+        :return:
+        """
+        return self.sets
+
+    def is_connect(self, i: int, j: int) -> bool:
+        """
+        判断两个元素是否连通
+        :param i: 第一个元素的索引
+        :param j: 第二个元素的索引
+        :return: 布尔值表示是否连通
+        """
+        if i > (self.count - 1) or j > (self.count - 1) or i < 0 or j < 0: return False  # 避免越界
+        if self.find(i) == self.find(j):
+            return True
+        else:
+            return False
+
+    def find(self, i: int) -> int:
+        if i < 0 or i > self.count - 1:
+            return -2
+        while self.elements[i] != -1:
+            i = self.elements[i]
+        return i
+
+    def union(self, i: int, j: int) -> bool:
+        if i > (self.count - 1) or j > (self.count - 1) or i < 0 or j < 0: return False  # 避免越界
+        if self.is_connect(i, j): return True
+        self.elements[i] = j  # i指向j
+        self.sets=self.sets-1
+
+    def print_union(self) -> None:
+        flags = []
+        for i in range(0, self.get_count()):
+            flags.append(0)
+        sets = self.get_sets()
+        for i in range(0, sets):
+            point = 0
+            for j in range(0, self.get_count()):
+                if self.elements[j] == -1 and flags[j] != 1:
+                    point = j
+                    print("\n集合：",end=" ")
+                    print(str(j),end=" ")
+                    flags[j]=1
+                    self.DFS(point,flags)
+
+    def DFS(self, point,flags):
+        queen = []
+        for i in range(self.get_count()):
+            if self.elements[i] == point and flags[i]!=1:
+                print(str(i),end=" ")
+                flags[i]=1
+                queen.append(i)
+        for i in range(0, len(queen)):
+            self.DFS(queen[i],flags)
+        return None
+
+
 if __name__ == '__main__':
-    u = UnionFind(10)  # 0 1 2 3 4 5 6 7 8 9 10
+    u = UnionFindQuickFind(10)  # 0 1 2 3 4 5 6 7 8 9 10
     # 0 1 2 3 4 5 6 7 8 9 10
     u.union(1, 8)  # 0 1 2 3 4 5 6 7 8 9 10
     # 0 1 2 3 4 5 6 7 1 9 10
@@ -92,4 +171,13 @@ if __name__ == '__main__':
     # 0 1 2 3 3 0 6 7 1 1 2
     u.union(6, 7)  # 0 1 2 3 4 5 6 7 8 9 10
     # 0 1 2 3 3 0 6 6 1 1 2
-    u.print_union()
+    #u.print_union()
+    uqf=UnionFindQuickUnion(9)
+    uqf.union(1,7)
+    uqf.union(4,7)
+    uqf.union(3,4)
+    uqf.union(2,4)
+    uqf.union(0,3)
+    uqf.union(5,8)
+    uqf.union(6,8)
+    uqf.print_union()
